4cbf2747e33f658c54949a9458c5579ed9ea43a2
memo figueredo <memof80@gmail.com>
2019-04-19 19:46:22 -0500

    Initial Commit

---
+++ Slide.swift
@@ -0,0 +1,19 @@
+//
+//  Slide.swift
+//  Slider
+//
+//  Created by Memo Figueredo on 4/19/19.
+//  Copyright © 2019 DeTodoUnPoquito. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelDes: UILabel!
+    
+}
---
+++ Slider/ViewController.swift
@@ -0,0 +1,122 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Memo Figueredo on 4/19/19.
+//  Copyright © 2019 DeTodoUnPoquito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    var slides:[Slide] = [];
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollview.delegate = self
+       
+        slides = createSlider()
+        setupSlideScrollView(slides: slides)
+        
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubviewToFront(pageControl)
+    }
+
+    @IBOutlet weak var scrollview: UIScrollView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+        
+        let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
+        let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
+        
+        // vertical
+        let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+        let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
+        
+        let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
+        let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
+        
+        
+        /*
+         * below code changes the background color of view on paging the scrollview
+         */
+        //        self.scrollView(scrollView, didScrollToPercentageOffset: percentageHorizontalOffset)
+        
+        
+        /*
+         * below code scales the imageview on paging the scrollview
+         */
+        let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
+        
+        if(percentOffset.x > 0 && percentOffset.x <= 0.25) {
+            
+            slides[0].imageView.transform = CGAffineTransform(scaleX: (0.25-percentOffset.x)/0.25, y: (0.25-percentOffset.x)/0.25)
+            slides[1].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.25, y: percentOffset.x/0.25)
+            
+        } else if(percentOffset.x > 0.25 && percentOffset.x <= 0.50) {
+            slides[1].imageView.transform = CGAffineTransform(scaleX: (0.50-percentOffset.x)/0.25, y: (0.50-percentOffset.x)/0.25)
+            slides[2].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.50, y: percentOffset.x/0.50)
+            
+        } else if(percentOffset.x > 0.50 && percentOffset.x <= 0.75) {
+            slides[2].imageView.transform = CGAffineTransform(scaleX: (0.75-percentOffset.x)/0.25, y: (0.75-percentOffset.x)/0.25)
+            slides[3].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.75, y: percentOffset.x/0.75)
+            
+        } else if(percentOffset.x > 0.75 && percentOffset.x <= 1) {
+            slides[3].imageView.transform = CGAffineTransform(scaleX: (1-percentOffset.x)/0.25, y: (1-percentOffset.x)/0.25)
+            slides[4].imageView.transform = CGAffineTransform(scaleX: percentOffset.x, y: percentOffset.x)
+        }
+    }
+   
+    
+    func setupSlideScrollView(slides : [Slide]) {
+        scrollview.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollview.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollview.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollview.addSubview(slides[i])
+        }
+    }
+    
+    func createSlider()-> [Slide]{
+        
+        let slide1: Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide1.imageView.image = UIImage(named: "ic_onboarding_1")
+        slide1.labelTitle.text = " A real Life bear"
+        slide1.labelDes.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide2.imageView.image = UIImage(named: "ic_onboarding_2")
+        slide2.labelTitle.text = "A real-life bear"
+        slide2.labelDes.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide3.imageView.image = UIImage(named: "ic_onboarding_3")
+        slide3.labelTitle.text = "A real-life bear"
+        slide3.labelDes.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        let slide4:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide4.imageView.image = UIImage(named: "ic_onboarding_4")
+        slide4.labelTitle.text = "A real-life bear"
+        slide4.labelDes.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        
+        let slide5:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide5.imageView.image = UIImage(named: "ic_onboarding_5")
+        slide5.labelTitle.text = "A real-life bear"
+        slide5.labelDes.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        return [slide1, slide2, slide3, slide4, slide5]
+    }
+    
+    
+}
+
